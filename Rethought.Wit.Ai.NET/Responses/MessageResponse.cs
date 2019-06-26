882a02e548fd33a470872f3fad9f90c339173a4f
Brian Ebeling <bje@live.de>
2019-06-26 13:11:43 +0200

    Refactorings and QoL updates

--- Rethought.Wit.Ai.NET/Responses/MessageResponse.cs
+++ Rethought.Wit.Ai.NET/Responses/MessageResponse.cs
@@ -1,6 +1,9 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
+using Rethought.Optional;
+using Rethought.Wit.Ai.NET.Responses.Entities;
 
 namespace Rethought.Wit.Ai.NET.Responses
 {
@@ -10,13 +13,23 @@ namespace Rethought.Wit.Ai.NET.Responses
         public IDictionary<string, IReadOnlyList<JObject>> Entities { get; set; } =
             new Dictionary<string, IReadOnlyList<JObject>>();
 
-        [JsonProperty("msg_id")] public string MessageId { get; set; }
+        [JsonProperty("msg_id")]
+        public string MessageId { get; set; }
 
-        [JsonProperty("_text")] public string Text { get; set; }
+        [JsonProperty("_text")]
+        public string Text { get; set; }
 
-        public IReadOnlyList<JObject> GetIntents()
+        public Option<IReadOnlyList<WitAiIntent>> GetIntents()
         {
-            return Entities.TryGetValue("intent", out var value) ? value : null;
+            return this.GetEntities<WitAiIntent>("intent");
+        }
+
+        public Option<IReadOnlyList<T>> GetEntities<T>(string name)
+            where T : Entity
+        {
+            return this.Entities.TryGetValue(name, out var entities)
+                ? entities.Select(x => x.ToObject<T>()).ToList().Some<IReadOnlyList<T>>()
+                : default;
         }
     }
 }
--- Rethought.Wit.Ai.NET/WitAiClient.cs
+++ Rethought.Wit.Ai.NET/WitAiClient.cs
@@ -11,32 +11,23 @@ using Rethought.Wit.Ai.NET.Responses;
 
 namespace Rethought.Wit.Ai.NET
 {
-    public class WitAiClient : INaturalLanguageProcessor
+    internal class ApiClient
     {
         private readonly IRequestUrlBuilder builder;
         private readonly HttpClient httpClient;
 
-        private WitAiClient(HttpClient httpClient, IRequestUrlBuilder builder)
+        private ApiClient(HttpClient httpClient, IRequestUrlBuilder builder)
         {
             this.httpClient = httpClient;
             this.builder = builder;
         }
 
-        public async Task<MessageResponse> GetMeaning(string message)
+        public static ApiClient Create(HttpClient httpClient, string token, string version = "20170307")
         {
-            //if (message.Length < 1 || message.Length >= 256) return null;
+            httpClient.BaseAddress = new Uri(ApiEndPoints.BaseUrl);
+            httpClient.DefaultRequestHeaders.Add("Authorization", "Bearer " + token);
 
-            var request = await Request<MessageResponse>(
-                builder.Build(
-                    ApiEndPoints.Message,
-                    new List<IParameter> {new Parameter("q", message)})).ConfigureAwait(false);
-
-            return request;
-        }
-
-        public static WitAiClient Create(HttpClient httpClient, string token, string version = "20170307")
-        {
-            var client = new WitAiClient(
+            var apiClient = new ApiClient(
                 httpClient,
                 new RequestUrlBuilder(
                     ApiEndPoints.BaseUrl,
@@ -45,10 +36,59 @@ namespace Rethought.Wit.Ai.NET
                         new Parameter("v", version)
                     }));
 
-            httpClient.BaseAddress = new Uri(ApiEndPoints.BaseUrl);
-            httpClient.DefaultRequestHeaders.Add("Authorization", "Bearer " + token);
+            return apiClient;
+        }
 
-            return client;
+        public async Task<T> Request<T>(Func<IRequestUrlBuilder, string> requestBuilder)
+        {
+            var response = await httpClient.GetAsync(requestBuilder.Invoke(builder)).ConfigureAwait(false);
+
+            var responseText = await response.Content.ReadAsStringAsync().ConfigureAwait(false);
+
+            Console.WriteLine(responseText);
+
+            return JsonConvert.DeserializeObject<T>(responseText);
+        }
+    }
+
+    public class WitAiClient
+    {
+        private readonly WitAiTrainingClient witAiTrainingClient;
+
+        private WitAiClient(WitAiTrainingClient witAiTrainingClient)
+        {
+            this.witAiTrainingClient = witAiTrainingClient;
+        }
+
+        public WitAiTrainingClient Training { get; set; }
+
+        public static WitAiClient Create(HttpClient httpClient, string token, string version = "20170307")
+        {
+            var apiClient = ApiClient.Create(httpClient, token, version);
+
+            return new WitAiClient(new WitAiTrainingClient(apiClient));
+        }
+    }
+
+    public class WitAiTrainingClient : INaturalLanguageProcessor
+    {
+        private readonly ApiClient apiClient;
+
+        internal WitAiTrainingClient(ApiClient apiClient)
+        {
+            this.apiClient = apiClient;
+        }
+
+        public async Task<MessageResponse> GetMeaning(string message)
+        {
+            //if (message.Length < 1 || message.Length >= 256) return null;
+
+            var request = await apiClient.Request<MessageResponse>(
+                builder => builder.Build(
+                    ApiEndPoints.Message,
+                    new List<IParameter> {new Parameter("q", message)})).ConfigureAwait(false);
+
+            return request;
         }
 
         public async Task<MessageResponse> GetMeaning(string message, bool verbose)
@@ -56,24 +96,12 @@ namespace Rethought.Wit.Ai.NET
             // TODO Verbose
             //if (message.Length < 1 || message.Length >= 256) return null;
 
-            var request = await Request<MessageResponse>(
-                builder.Build(
+            var request = await apiClient.Request<MessageResponse>(
+                builder => builder.Build(
                     ApiEndPoints.Message,
                     new List<IParameter> {new Parameter("q", message)})).ConfigureAwait(false);
 
             return request;
-        }
-
-
-        private async Task<T> Request<T>(string request)
-        {
-            var response = await httpClient.GetAsync(request).ConfigureAwait(false);
-
-            var responseText = await response.Content.ReadAsStringAsync().ConfigureAwait(false);
-
-            Console.WriteLine(responseText);
-
-            return JsonConvert.DeserializeObject<T>(responseText);
         }
     }
 }
